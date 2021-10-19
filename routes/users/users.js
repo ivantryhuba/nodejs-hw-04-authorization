7293c067e9e61d5e68a47f7d2f0c2bb484d78ddf
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-10-19 22:53:36 +0300

    create elementary sequrity

--- routes/users/users.js
+++ routes/users/users.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { registration, login, logout } = require('../../controllers/users');
 const guard = require('../../helpers/guard');
+const loginLimit = require('../../helpers/rateLimitLogin');
 
-router.post('/registration', registration);
-router.post('/login', login);
+router.post('/signup', registration);
+router.post('/login',loginLimit, login);
 router.post('/logout', guard, logout);
 
 module.exports = router;
